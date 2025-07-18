4947a4a399471d61064d1ebb3d80127218635f2d
Fennelfoxxo <fennelfoxxo@gmail.com>
2025-07-18 11:36:01 -0700

    updated case of Tailspring to tailspring

--- Findtailspring.cmake
+++ Findtailspring.cmake
@@ -2,5 +2,5 @@ include_guard(GLOBAL)
 
 set(TAILSPRING_CONFIG_FILENAME "tailspringconfig.yaml" CACHE STRING "Name of the Tailspring configuration file in runtime-configs/")
 
-add_subdirectory("${CMAKE_CURRENT_LIST_DIR}" Tailspring)
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}" tailspring)
 add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib" tailspring_lib)
